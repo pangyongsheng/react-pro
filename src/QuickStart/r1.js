2f6754c55d2ca9bceb18f7571dc9b1bce8ed83d0
pangyongsheng <pang_yongsheng@outlook.com>
2018-09-12 15:06:24 +0800

    add router

--- src/QuickStart/r1.js
+++ src/QuickStart/r1.js
@@ -10,7 +10,7 @@ export default class Aa extends Component {
         return <Router>
         	<nav>
         		<Link to="/">首页</Link>
-		   		 <Link to="/b">电影</Link>
+		   		<Link to="/b">电影</Link>
 		    	<Link to="/c">关于</Link>
 		    	<Route exact path="/" component={Ca}></Route>
 		    	<Route path="/b" component={Cb}></Route>
